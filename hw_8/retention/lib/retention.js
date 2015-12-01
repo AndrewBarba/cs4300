d7318ce25cbb45f4df7820f95acdf9099139988d
Andrew Barba <abarba.77@gmail.com>
2015-11-30 22:20:05 -0500

    es6

--- hw_8/retention/lib/retention.js
+++ hw_8/retention/lib/retention.js
@@ -1,16 +1,9 @@
-/*============================================================================*
- *                                                                            *
- *                                 Retention                                  *
- *                                                                            *
- *============================================================================*/
-
-/*============================================================================*
- * Dependencies                                                               *
- *============================================================================*/
+"use strict";
 
 const async = require('async');
 const dateUtils = require('./date-utils');
 
+// Constants
 const DATE_FUNCTIONS = {
     day: [ dateUtils.diaryDate, dateUtils.tomorrow ],
    week: [ dateUtils.diaryWeek, dateUtils.nextWeek ],
@@ -18,122 +11,122 @@ const DATE_FUNCTIONS = {
    year: [ dateUtils.diaryYear, dateUtils.nextYear ]
 };
 
-/*============================================================================*
- * Service                                                                    *
- *============================================================================*/
+class Retention {
 
-function Retention(period, sections, init, then) {
-	this.data = {};
-	this.sections = sections;
-	this.period = period;
-	this.init = init;
-	this.then = then;
+  constructor(period, sections, init, then) {
+    this._data = {};
+  	this._sections = sections;
+  	this._period = period;
+  	this._init = init;
+  	this._then = then;
+  }
+
+  get data() { return this._data; }
+  get sections() { return this._sections; }
+  get period() { return this._period; }
+  get init() { return this._init; }
+  get then() { return this._then; }
+
+  generate(next) {
+  	let period = this.period;
+  	let sections = this.sections;
+  	let data = this.data;
+
+  	// build data
+  	async.timesSeries(sections, (section, next) => {
+  		let query = this.initDateQuery(period, section);
+  		let sectionDate = this.sectionDate(period, section);
+  		this.init(query, function(err, ids){
+  			data[section] = [{
+  				count: ids.length,
+  				ids: ids,
+  				date: sectionDate
+  			}];
+  			next();
+  		});
+  	}, () => {
+  		async.timesSeries(sections, (section, next) => {
+  			async.timesSeries(section + 1, (slot, next) => {
+  				let init = data[section][0];
+  				let query = this.thenDateQuery(init.date, period, slot + 1);
+  				let ids = init.ids;
+  				this.then(query, ids, (err, ids) => {
+  					data[section].push({
+  						query: query,
+  						count: ids.length,
+  						ids: ids,
+  						percentage: (ids.length / init.count) * 100
+  					});
+  					next();
+  				});
+  			}, next);
+  		}, () => {
+  			next(null, this);
+  		});
+  	});
+  }
+
+  print() {
+  	let result = '';
+
+  	let initLine = [ this.period, 'initial' ];
+  	for (let i = 1; i <= this.sections; i++) {
+  		initLine.push(i + ' ' + this.period);
+  	}
+  	result += initLine.join(',') + '\n';
+
+  	for (let i2 = this.sections; i2 > 0; i2--) {
+  		let sec = this.data[i2-1];
+  		let date = dateUtils.excelDateFormat(sec[0].date);
+  		let line = [ date, sec[0].count ];
+  		for (let x = 1; x < sec.length; x++) {
+  			let point = sec[x];
+  			let val = isNaN(point.percentage) ? 0.0 : point.percentage;
+  			line.push(val.toFixed(1) + "%");
+  		}
+  		result += line.join(',') + '\n';
+  	}
+
+  	return result.trim();
+  }
+
+  initDateQuery(period, section) {
+  	let getDate = DATE_FUNCTIONS[period][0];
+  	let getNextDate = DATE_FUNCTIONS[period][1];
+
+  	let now = getDate();
+  	let lt = getNextDate(now, -(section + 0));
+  	let gte = getNextDate(now, -(section + 1));
+
+    return {
+      created: {
+        $gte: gte,
+        $lt: lt
+      }
+  	};
+  }
+
+  thenDateQuery(date, period, section) {
+  	let getNextDate = DATE_FUNCTIONS[period][1];
+
+    let gte = getNextDate(date, section);
+  	let lt = getNextDate(date, section + 1);
+
+    return {
+      created: {
+        $gte: gte,
+        $lt: lt
+      }
+  	};
+  }
+
+  sectionDate(period, section) {
+  	let getDate = DATE_FUNCTIONS[period][0];
+  	let getNextDate = DATE_FUNCTIONS[period][1];
+  	let now = getDate();
+  	return getNextDate(now, -(section + 1));
+  }
 }
-
-/*============================================================================*
- * Methods                                                                    *
- *============================================================================*/
-
-Retention.prototype.generate = function(next) {
-	var _this = this;
-	var period = this.period;
-	var sections = this.sections;
-	var data = this.data;
-
-	// build data
-	async.timesSeries(sections, function(section, next){
-		var query = _this.initDateQuery(period, section);
-		var sectionDate = _this.sectionDate(period, section);
-		_this.init(query, function(err, ids){
-			data[section] = [{
-				count: ids.length,
-				ids: ids,
-				date: sectionDate
-			}];
-			next();
-		});
-	}, function(){
-		async.timesSeries(sections, function(section, next){
-			async.timesSeries(section + 1, function(slot, next){
-				var init = data[section][0];
-				var query = _this.thenDateQuery(init.date, period, slot + 1);
-				var ids = init.ids;
-				_this.then(query, ids, function(err, ids){
-					data[section].push({
-						query: query,
-						count: ids.length,
-						ids: ids,
-						percentage: (ids.length / init.count) * 100
-					});
-					next();
-				});
-			}, next);
-		}, function(){
-			next(null, _this);
-		});
-	});
-};
-
-Retention.prototype.print = function() {
-	var result = '';
-
-	var initLine = [ this.period, 'initial' ];
-	for (var i = 1; i <= this.sections; i++) {
-		initLine.push(i + ' ' + this.period);
-	}
-	result += initLine.join(',') + '\n';
-
-	for (var i2 = this.sections; i2 > 0; i2--) {
-		var sec = this.data[i2-1];
-		var date = dateUtils.excelDateFormat(sec[0].date);
-		var line = [ date, sec[0].count ];
-		for (var x = 1; x < sec.length; x++) {
-			var point = sec[x];
-			var val = isNaN(point.percentage) ? 0.0 : point.percentage;
-			line.push(val.toFixed(1) + "%");
-		}
-		result += line.join(',') + '\n';
-	}
-
-	return result.trim();
-};
-
-Retention.prototype.initDateQuery = function(period, section) {
-	var getDate = DATE_FUNCTIONS[period][0];
-	var getNextDate = DATE_FUNCTIONS[period][1];
-
-	var now = getDate();
-	var lt = getNextDate(now, -(section + 0));
-	var gte = getNextDate(now, -(section + 1));
-
-  return {
-    created: {
-      $gte: gte,
-      $lt: lt
-    }
-	};
-};
-
-Retention.prototype.thenDateQuery = function(date, period, section) {
-	var getNextDate = DATE_FUNCTIONS[period][1];
-
-  var gte = getNextDate(date, section);
-	var lt = getNextDate(date, section + 1);
-
-  return {
-    created: {
-      $gte: gte,
-      $lt: lt
-    }
-	};
-};
-
-Retention.prototype.sectionDate = function(period, section) {
-	var getDate = DATE_FUNCTIONS[period][0];
-	var getNextDate = DATE_FUNCTIONS[period][1];
-	var now = getDate();
-	return getNextDate(now, -(section + 1));
-};
 
 /*============================================================================*
  * Expose                                                                     *
@@ -145,8 +138,8 @@ exports = module.exports = Retention;
  * Static                                                                     *
  *============================================================================*/
 
-exports.generate = function(period, sections, init, then, next) {
-	var ret = new Retention(period, sections, init, then);
+exports.generate = (period, sections, init, then, next) => {
+	let ret = new Retention(period, sections, init, then);
 	ret.generate(next);
 	return ret;
 };
